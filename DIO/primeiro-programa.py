98d721ce4bb1dd051d93bf3e12745ea5ba3c93c1
Kelvin Clenderson <kelvinclenderson95@gmail.com>
2023-02-28 14:42:46 -0300

    Update primeiro-programa.py

--- DIO/primeiro-programa.py
+++ DIO/primeiro-programa.py
@@ -1,2 +1,36 @@
-#   Primeiro projeto Python
-print ("Olá mundo")
+#   Sistema Bancário
+
+menu = """
+
+[d] Depositar
+[s] Sacar
+[e] Extrato
+[q] Sair
+
+>= """
+
+saldo = 0
+limite = 500
+extrato = ""
+numero_saques = 0
+LIMITE_SAQUES = 3
+
+while True:
+
+    opcao = input(menu)
+
+    if opcao == "d":
+        print("Depósito")
+
+    elif opcao == "s":
+        print ("Sacar")
+
+    elif opcao == "e":
+        print("Extrato")
+
+    elif opcao == "q":
+        break
+
+    else:
+        print("Operação inválida, por favor selecione novamente a operação desejada.")
+        
